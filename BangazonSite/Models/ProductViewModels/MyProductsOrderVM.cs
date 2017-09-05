8a312eb2fa631b276d29898ad81d9668d25fe0e4
Jordan <jordan.dhaenens@gmail.com>
2017-09-05 13:40:36 -0500

    Ticket features complete. Need to merge the current master into this branch before PR

--- BangazonSite/Models/ProductViewModels/MyProductsOrderVM.cs
+++ BangazonSite/Models/ProductViewModels/MyProductsOrderVM.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using BangazonSite.Models;
 using BangazonSite.Data;
 using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace BangazonSite.Models.ProductViewModels
 {
@@ -13,6 +14,9 @@ namespace BangazonSite.Models.ProductViewModels
         public string Title { get; set; }
         public int Quantity { get; set; }
         public int OrderedCount { get; set; }
+        public int ProductId { get; set; }
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        public double Price { get; set; }
     }
 
     // This class was authored by Jordan Dhaenens
@@ -26,18 +30,20 @@ namespace BangazonSite.Models.ProductViewModels
         {
             User = user;
             Products = ctx.Product.Where(p => p.User == user).Include(t => t.ProductType);
-           
+
             UserProducts = (
                 from p in ctx.Product
+                where p.User == user
                 join op in ctx.OrderProduct
                 on p.ProductId equals op.ProductId
-                group new { p, op } by new { p.ProductId, p.Quantity, p.Title } into grouped
+                group new { p, op } by new { p.ProductId, p.Quantity, p.Title, p.Price } into grouped
                 select new GroupedProducts
                 {
                     Title = grouped.Key.Title,
-                    Quantity = grouped.Key.Quantity,
                     OrderedCount = grouped.Select(x => x.op.ProductId).Count(),
-                    
+                    Quantity = (grouped.Key.Quantity - grouped.Select(x => x.op.ProductId).Count()),
+                    ProductId = grouped.Key.ProductId,
+                    Price = grouped.Key.Price
                 }).ToList();
 
             
